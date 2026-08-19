66c3bbc9c64090ffea7611c80711bc58455d57a4
Pradyumn Agrahari <pradyumnagrahari111@gmail.com>
2026-08-19 18:03:54 +0530

    commit

--- Document/pyPdf.py
+++ Document/pyPdf.py
@@ -1,1 +1,4 @@
 from langchain_community.document_loaders import PyPDFLoader
+loader=PyPDFLoader("sample.pdf")
+docs=loader.load()  
+print(docs[0].page_content)
--- Document/text_loader.py
+++ Document/text_loader.py
@@ -1,5 +1,32 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+import os
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableParallel
 
-loader=TextLoader('file.txt')
-docs=loader.load()
-print(docs)
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    temperature=0.7,
+)
+
+model=ChatHuggingFace(llm=llm)
+
+prompt=PromptTemplate(
+    template='Write a sumary on the poem {topic}',
+    input_variables=['topic']
+)
+
+parser=StrOutputParser()
+chain=prompt|model|parser
+
+
+loader = TextLoader("file.txt")
+docs = loader.load()
+ans=chain.invoke({'topic':docs[0].page_content})
+
+print(ans)
